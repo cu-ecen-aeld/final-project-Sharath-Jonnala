554c01ef14e6471edcc0327d37f76a3e69403a00
Sharath-Jonnala <shjo6077@colorado.edu>
2021-11-14 16:29:09 -0700

    Sprint 2 delivery - GPIO enable and testing

--- base_external/package/gpio/gpio.mk
+++ base_external/package/gpio/gpio.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-GPIO_VERSION = 'ddcaed8360a46db24ae2cda9163cbd846929b2b1'
+GPIO_VERSION = 'e0456c1ff51232b98641080892f72805c562f06e'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -15,16 +15,13 @@ GPIO_SITE_METHOD = git
 GPIO_GIT_SUBMODULES = YES
 
 define GPIO_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/gpio_test all
 endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
 define GPIO_INSTALL_TARGET_CMDS
 
-	$(INSTALL) -m 0755 $(@D)/Makefile $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 0755 $(@D)/test_gpio_read.c $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 0755 $(@D)/test_gpio_write.c $(TARGET_DIR)/usr/bin
-	
+	$(INSTALL) -m 0755 $(@D)/gpio_test/* $(TARGET_DIR)/usr/bin
 	
 endef
 
