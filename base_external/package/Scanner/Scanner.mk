8745bad4ed2cd982f033c2f41fe6476fada5c852
Sharath-Jonnala <shjo6077@colorado.edu>
2021-11-27 23:16:43 -0700

    Transmitter Side complete

---
+++ base_external/package/Scanner/Scanner.mk
@@ -0,0 +1,28 @@
+
+##############################################################
+#
+# FINAL-PROJECT SCANNER FUNCTIONALITY
+#
+##############################################################
+
+#TODO: Fill up the contents below in order to reference your assignment 3 git contents
+SCANNER_VERSION = '3627b10bde48f75c4c15e6f50bea13c38cc0b097'
+# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
+# with ssh keys and the automated build/test system.
+# Your site should start with git@github.com:
+SCANNER_SITE = 'https://github.com/cu-ecen-aeld/final-project-chaithra-sharath.git'
+SCANNER_SITE_METHOD = git
+SCANNER_GIT_SUBMODULES = YES
+
+define SCANNER_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/2d-scanner all
+endef
+
+# TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
+define SCANNER_INSTALL_TARGET_CMDS
+
+	$(INSTALL) -m 0755 $(@D)/2d-scanner/* $(TARGET_DIR)/usr/bin
+	
+endef
+
+$(eval $(generic-package))
